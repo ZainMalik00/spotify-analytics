24b5a46eda165951c21449f172f8dccc7dec5e5c
ZainMalik00 <zmmalik00@gmail.com>
2023-11-25 02:24:06 -0500

    Fixed Incorrect GitHub Redirect URI

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 
 const CLIENT_ID = "1b82855ba97c408590be267aa16378b2";
 const SPOTIFY_AUTH = "https://accounts.spotify.com/authorize?";
-const REDIRECT_URI = "https://github.com/ZainMalik00/spotify-analytics/";
+const REDIRECT_URI = "https://zainmalik00.github.io/spotify-analytics/";
 const SPACE_DELIMITER = "%20";
 const SCOPES = ["user-top-read", "user-read-recently-played"];
 const SCOPES_URL = SCOPES.join(SPACE_DELIMITER);
