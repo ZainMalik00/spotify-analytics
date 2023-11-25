dfa0b4dc883aab25fd97d06caef5addd402813b6
ZainMalik00 <zmmalik00@gmail.com>
2023-11-25 13:55:50 -0500

    Refactored App.js

--- src/App.js
+++ src/App.js
@@ -24,12 +24,12 @@ function App() {
     if(window.location.hash) {
       const spotifyAccessParams = getParamsAfterLoginRedirect(window.location.hash);
       if(spotifyAccessParams.access_token.length != 0){
-        updateUserTopArtistsShortTerm(spotifyAccessParams.access_token);
-        updateUserTopArtistsMediumTerm(spotifyAccessParams.access_token);
-        updateUserTopArtistsLongTerm(spotifyAccessParams.access_token);
-        updateUserTopTracksShortTerm(spotifyAccessParams.access_token);
-        updateUserTopTracksMediumTerm(spotifyAccessParams.access_token);
-        updateUserTopTracksLongTerm(spotifyAccessParams.access_token);
+        updateUserTopArtists(spotifyAccessParams.access_token, 'SHORT');
+        updateUserTopArtists(spotifyAccessParams.access_token, 'MEDIUM');
+        updateUserTopArtists(spotifyAccessParams.access_token, 'LONG');
+        updateUserTopTracks(spotifyAccessParams.access_token, 'SHORT');
+        updateUserTopTracks(spotifyAccessParams.access_token, 'MEDIUM');
+        updateUserTopTracks(spotifyAccessParams.access_token, 'LONG');
         updateUserMostRecentTracks(spotifyAccessParams.access_token);
       }
     }
@@ -51,113 +51,105 @@ function App() {
     );
   }
 
-  const updateUserTopArtistsShortTerm = (spotifyAccessToken) => {
-    var spotifyTopArtistsShortTermGET = {
+  const getArtistEndpoint = (timeRange) => {
+    switch (timeRange){
+      case 'SHORT': return 'https://api.spotify.com/v1/me/top/artists?time_range=short_term&limit=50&offset=0';
+        break;
+      case 'MEDIUM': return  'https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=50&offset=0';
+        break;
+      case 'LONG': return 'https://api.spotify.com/v1/me/top/artists?time_range=long_term&limit=50&offset=0';
+        break;
+      default: return  'https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=50&offset=0';
+        break;
+    };
+  };
+
+  const getTrackEndpoint = (timeRange) => {
+    switch (timeRange){
+      case 'SHORT': return 'https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=50&offset=0';
+        break;
+      case 'MEDIUM': return  'https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=50&offset=0';
+        break;
+      case 'LONG': return 'https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=50&offset=0';
+        break;
+      default: return  'https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=50&offset=0';
+        break;
+    };
+  };
+
+  const appendUserTopArtistsState = (artist, timeRange) => {
+    switch (timeRange){
+      case 'SHORT':
+        setUserTopArtistsShortTerm(userTopArtistsShortTerm => [...userTopArtistsShortTerm, {"id": artist.id, "name": artist.name}]);
+        break;
+      case 'MEDIUM':
+        setUserTopArtistsMediumTerm(userTopArtistsMediumTerm => [...userTopArtistsMediumTerm, {"id": artist.id, "name": artist.name}]);
+        break;
+      case 'LONG':
+        setUserTopArtistsLongTerm(userTopArtistsLongTerm => [...userTopArtistsLongTerm, {"id": artist.id, "name": artist.name}]);
+        break;
+      default:
+        setUserTopArtistsMediumTerm(userTopArtistsMediumTerm => [...userTopArtistsMediumTerm, {"id": artist.id, "name": artist.name}]);
+        break;
+    };
+  };
+
+  const appendUserTopTracksState = (track, artistsListString, timeRange) => {
+    switch (timeRange){
+      case 'SHORT':
+        setUserTopTracksShortTerm(userTopTracksShortTerm => [...userTopTracksShortTerm, {"id": track.id, "name": track.name, "artist": artistsListString}]);
+        break;
+      case 'MEDIUM':
+        setUserTopTracksMediumTerm(userTopTracksMediumTerm => [...userTopTracksMediumTerm, {"id": track.id, "name": track.name, "artist": artistsListString}]);
+        break;
+      case 'LONG':
+        setUserTopTracksLongTerm(userTopTracksLongTerm => [...userTopTracksLongTerm, {"id": track.id, "name": track.name, "artist": artistsListString}]);
+        break;
+      default:
+        setUserTopTracksMediumTerm(userTopTracksMediumTerm => [...userTopTracksMediumTerm, {"id": track.id, "name": track.name, "artist": artistsListString}]);
+        break;
+    };
+  };
+
+  const generateArtistListString = (artists) => {
+    let artistsList = [];
+    artists.forEach((artist) => {
+      artistsList.push(artist.name);
+    });
+    return artistsList.join(", ");  
+  };
+
+  const updateUserTopArtists = (spotifyAccessToken, timeRange) => {
+    let artistEndpoint = getArtistEndpoint(timeRange);
+    var spotifyTopArtistsGET = {
       method: 'GET',
       headers: {
         'Authorization': 'Bearer ' + spotifyAccessToken
       }
     }
-    fetch('https://api.spotify.com/v1/me/top/artists?time_range=short_term&limit=50&offset=0', spotifyTopArtistsShortTermGET)
+    fetch(artistEndpoint, spotifyTopArtistsGET)
       .then(response => response.json())
       .then((responseJSON) => { 
-          responseJSON.items.forEach(artistItem => {
-          setUserTopArtistsShortTerm(userTopArtistsShortTerm => [...userTopArtistsShortTerm, {"id": artistItem.id, "name": artistItem.name}]);  
+        responseJSON.items.forEach(artistItem => {
+          appendUserTopArtistsState(artistItem, timeRange);  
           })
         });
   };
 
-  const updateUserTopArtistsMediumTerm = (spotifyAccessToken) => {
-    var spotifyTopArtistsMediumTermGET = {
+  const updateUserTopTracks = (spotifyAccessToken, timeRange) => {
+    let artistEndpoint = getTrackEndpoint(timeRange);
+    var spotifyTopTracksGET = {
       method: 'GET',
       headers: {
         'Authorization': 'Bearer ' + spotifyAccessToken
       }
     }
-    fetch('https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=50&offset=0', spotifyTopArtistsMediumTermGET)
+    fetch(artistEndpoint, spotifyTopTracksGET)
       .then(response => response.json())
       .then((responseJSON) => { 
-          responseJSON.items.forEach(artistItem => {
-          setUserTopArtistsMediumTerm(userTopArtistsMediumTerm => [...userTopArtistsMediumTerm, {"id": artistItem.id, "name": artistItem.name}]);  
-          })
-        });
-  };
-
-  const updateUserTopArtistsLongTerm = (spotifyAccessToken) => {
-    var spotifyTopArtistsLongTermGET = {
-      method: 'GET',
-      headers: {
-        'Authorization': 'Bearer ' + spotifyAccessToken
-      }
-    }
-    fetch('https://api.spotify.com/v1/me/top/artists?time_range=long_term&limit=50&offset=0', spotifyTopArtistsLongTermGET)
-      .then(response => response.json())
-      .then((responseJSON) => { 
-          responseJSON.items.forEach(artistItem => {
-          setUserTopArtistsLongTerm(userTopArtistsLongTerm => [...userTopArtistsLongTerm, {"id": artistItem.id, "name": artistItem.name}]);  
-          })
-        });
-  };
-
-  const updateUserTopTracksShortTerm = (spotifyAccessToken) => {
-    var spotifyTopTracksShortTermGET = {
-      method: 'GET',
-      headers: {
-        'Authorization': 'Bearer ' + spotifyAccessToken
-      }
-    }
-    fetch('https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=50&offset=0', spotifyTopTracksShortTermGET)
-      .then(response => response.json())
-      .then((responseJSON) => { 
-          responseJSON.items.forEach(trackItem => {
-            let artistsList = [];
-            trackItem.artists.forEach((artist) => {
-              artistsList.push(artist.name);
-            });
-            const artistsListString = artistsList.join(", ");
-            setUserTopTracksShortTerm(userTopTracksShortTerm => [...userTopTracksShortTerm, {"id": trackItem.id, "name": trackItem.name, "artist": artistsListString}]);  
-          })
-        });
-  };
-
-  const updateUserTopTracksMediumTerm = (spotifyAccessToken) => {
-    var spotifyTopTracksMediumTermGET = {
-      method: 'GET',
-      headers: {
-        'Authorization': 'Bearer ' + spotifyAccessToken
-      }
-    }
-    fetch('https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=50&offset=0', spotifyTopTracksMediumTermGET)
-      .then(response => response.json())
-      .then((responseJSON) => { 
-          responseJSON.items.forEach(trackItem => {
-            let artistsList = [];
-            trackItem.artists.forEach((artist) => {
-              artistsList.push(artist.name);
-            });
-            const artistsListString = artistsList.join(", ");
-            setUserTopTracksMediumTerm(userTopTracksMediumTerm => [...userTopTracksMediumTerm, {"id": trackItem.id, "name": trackItem.name, "artist": artistsListString}]);  
-          })
-        });
-  };
-
-  const updateUserTopTracksLongTerm = (spotifyAccessToken) => {
-    var spotifyTopTracksLongTermGET = {
-      method: 'GET',
-      headers: {
-        'Authorization': 'Bearer ' + spotifyAccessToken
-      }
-    }
-    fetch('https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=50&offset=0', spotifyTopTracksLongTermGET)
-      .then(response => response.json())
-      .then((responseJSON) => { 
-          responseJSON.items.forEach(trackItem => {
-            let artistsList = [];
-            trackItem.artists.forEach((artist) => {
-              artistsList.push(artist.name);
-            });
-            const artistsListString = artistsList.join(", ");
-            setUserTopTracksLongTerm(userTopTracksLongTerm => [...userTopTracksLongTerm, {"id": trackItem.id, "name": trackItem.name, "artist": artistsListString}]);  
+        responseJSON.items.forEach(trackItem => {
+          const artistsListString = generateArtistListString(trackItem.artists);
+          appendUserTopTracksState(trackItem, artistsListString, timeRange);  
           })
         });
   };
@@ -173,51 +165,31 @@ function App() {
       .then(response => response.json())
       .then((responseJSON) => { 
           responseJSON.items.forEach((trackItem, index) => {
-            let artistsList = [];
-            trackItem.track.artists.forEach((artist) => {
-              artistsList.push(artist.name);
-            });
-            const artistsListString = artistsList.join(", ");
+            const artistsListString = generateArtistListString(trackItem.track.artists);
             setUserMostRecentTracks(userMostRecentTracks => [...userMostRecentTracks, {"id": trackItem.track.id.concat(index), "name": trackItem.track.name, "artist": artistsListString}]);  
           })
         });
   };
 
-  const listUserTopArtistsShortTermItems = userTopArtistsShortTerm.map((artist) =>
-    <li key={artist.id}>
-      {artist.name}
-    </li>
-  );
+  const generateListUserTopArtists = (userTopArtistsState) => {
+    return(
+      userTopArtistsState.map((artist) => {
+        <li key={artist.id}>
+          {artist.name}
+        </li> 
+      })
+    );
+  }
 
-  const listUserTopArtistsMediumTermItems = userTopArtistsMediumTerm.map((artist) =>
-    <li key={artist.id}>
-      {artist.name}
-    </li>
-  );
-
-  const listUserTopArtistsLongTermItems = userTopArtistsLongTerm.map((artist) =>
-    <li key={artist.id}>
-      {artist.name}
-    </li>
-  );
-
-  const listUserTopTracksShortTermItems = userTopTracksShortTerm.map((track) =>
-    <li key={track.id}>
-      {track.name} - {track.artist}
-    </li>
-  );
-
-  const listUserTopTracksMediumTermItems = userTopTracksMediumTerm.map((track) =>
-    <li key={track.id}>
-      {track.name} - {track.artist}
-    </li>
-  );
-
-  const listUserTopTracksLongTermItems = userTopTracksLongTerm.map((track) =>
-    <li key={track.id}>
-      {track.name} - {track.artist}
-    </li>
-  );
+  const generateListUserTopTracks = (userTopTracksState) => {
+    return(
+      userTopTracksState.map((track) => {
+        <li key={track.id}>
+          {track.name} - {track.artist}
+        </li> 
+      })
+    );
+  }
 
   const listUserMostRecentTracks = userMostRecentTracks.map((track) =>
     <li key={track.id}>
@@ -230,17 +202,17 @@ function App() {
       <button onClick={loginRedirect}>Login To Spotify</button>
       <div className="Body">
         <label>Top Artists - Short Term (4 Weeks)</label>
-        <ul>{listUserTopArtistsShortTermItems}</ul>
+        <ul>{generateListUserTopArtists(userTopArtistsShortTerm)}</ul>
         <label>Top Artists - Medium Term (6 Months)</label>
-        <ul>{listUserTopArtistsMediumTermItems}</ul>
+        <ul>{generateListUserTopArtists(userTopArtistsMediumTerm)}</ul>
         <label>Top Artists - Long Term (All Time)</label>
-        <ul>{listUserTopArtistsLongTermItems}</ul>
+        <ul>{generateListUserTopArtists(userTopArtistsLongTerm)}</ul>
         <label>Top Tracks - Short Term (4 Weeks)</label>
-        <ul>{listUserTopTracksShortTermItems}</ul>
+        <ul>{generateListUserTopTracks(userTopTracksShortTerm)}</ul>
         <label>Top Tracks - Medium Term (6 Months)</label>
-        <ul>{listUserTopTracksMediumTermItems}</ul>
+        <ul>{generateListUserTopTracks(userTopTracksMediumTerm)}</ul>
         <label>Top Artists - Long Term (All Time)</label>
-        <ul>{listUserTopTracksLongTermItems}</ul>
+        <ul>{generateListUserTopTracks(userTopTracksLongTerm)}</ul>
         <label>Most Recently Played</label>
         <ul>{listUserMostRecentTracks}</ul>
       </div>
