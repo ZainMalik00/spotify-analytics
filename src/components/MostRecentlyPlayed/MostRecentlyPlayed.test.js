712354e9bc05b449dc1999676f2864e7a672ae09
Zain <47285447+ZainMalik00@users.noreply.github.com>
2023-11-27 03:55:48 -0500

    Rebase Main into Dev (#10)
    * Updated API Calls to Only Occur On Login (#8)
    
    * Refactored App.js
    
    * Refactored App.js to Seperate Components
    
    * Refactored with services (WIP)
    
    * Fixed States not waiting for API
    
    * Adjusted Timings to Allow API to Fetch Data
    
    * Setup for PR
    
    * Added Contexts for User Data Held in New Container
    
    * Updated Redirect URI for Deployment 2
    
    * Removed Files That Should Be Deleted By PR

--- src/components/MostRecentlyPlayed/MostRecentlyPlayed.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import MostRecentlyPlayed from './MostRecentlyPlayed';
-
-test('renders learn react link', () => {
-  render(<MostRecentlyPlayed />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- src/components/TopTracksList/TopTrackList.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import TopTrackList from './TopTrackList';
-
-test('renders learn react link', () => {
-  render(<TopTrackList />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
