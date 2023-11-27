12c12a677f06929711cb8f65700058cdde9441a8
ZainMalik00 <zmmalik00@gmail.com>
2023-11-27 03:23:23 -0500

    Updated Redirect URI for Deployment 2

--- src/backend/services/login-service.js
+++ src/backend/services/login-service.js
@@ -1,7 +1,7 @@
 const CLIENT_ID = "1b82855ba97c408590be267aa16378b2";
 const SPOTIFY_AUTH = "https://accounts.spotify.com/authorize?";
-const REDIRECT_URI = "http://localhost:3000/";
-//const REDIRECT_URI = "https://zainmalik00.github.io/spotify-analytics/";
+//const REDIRECT_URI = "http://localhost:3000/";
+const REDIRECT_URI = "https://zainmalik00.github.io/spotify-analytics/";
 const SPACE_DELIMITER = "%20";
 const SCOPES = ["user-top-read", "user-read-recently-played"];
 const SCOPES_URL = SCOPES.join(SPACE_DELIMITER);
